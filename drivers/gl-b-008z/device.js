d3c674749a844b94bceabb33ab1bc69fbc03f191
Marco Ruiter <mruiter@tuxweb.nl>
2020-09-30 17:01:12 +0200

    Added devices

---
+++ drivers/gl-b-008z/device.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const Light = require("../Light.js");
+
+class gl-b-008z extends Light { }
+
+module.exports = gl-b-008z;
---
+++ drivers/gl-mc-001/device.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const Light = require("../Light.js");
+
+class gl-mc-001 extends Light { }
+
+module.exports = gl-mc-001;
